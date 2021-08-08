9b8ac18288061696e58ebc6f92a71c5bdc4e4c82
Jeong-Daniel <empcik@pusan.ac.kr>
2021-08-08 18:50:03 +0900

    GPS parsing

--- main_code/Rpi_GPS.py
+++ main_code/Rpi_GPS.py
@@ -1,28 +1,36 @@
 import serial
 import pynmea2
 import time
+#import libary
 
 port = '/dev/ttyAMA0'
 ser = serial.Serial(port, baudrate=9600)
 print("serial connect")
+#Using RPi UART 0 PORT
+#import port and bitrate 9600, not timeout)
 
 while True:
-    data = ser.readline()
-    print(data)
-    time.sleep(0.5)
+    data = ser.readline().decode()
+    #Import GPS data from serial port GPS
+    
+    # GPGGA is Global Positioning system fix data
+    if data[0:6] == '$GPGGA':
+        #parseing gps data, and decode string
+        msg = pynmea2.parse(data)
+        #latitude
+        latval = msg.latitude
+        #longitude
+        longval = msg.longitude
+        print('latitude :', latval)
+        print('longtidue :', longval)
 
+    time.sleep(0.5)
+    
 """
 while True:
     data = ser.readline()
-    if data[0:6] == '$GPGGA':
-        print(data)
+    temp = str(data)
+    if temp[2:8] == '$GPGGA':
+        print(temp)
     time.sleep(0.5)
-
-while True:
-    data = ser.readline()
-    if data[0:6] == '$GPGGA':
-        msg = pynmea2.parse(data)
-        print("latitude: " + msg.lat+msg.lat_dir)
-        print("longitude: " + msg.lon+msg.lon_dir)
-        print("altidue: "+str(msg.altitude)+msg.altitude_units)
 """
